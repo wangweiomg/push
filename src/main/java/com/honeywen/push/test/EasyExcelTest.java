21060d735c224ac28d60943591aaf9fdd869154e
weiw <weiw@opera.com>
2019-04-24 12:25:42 +0800

    add some utils

---
+++ src/main/java/com/honeywen/push/test/EasyExcelTest.java
@@ -0,0 +1,12 @@
+package com.honeywen.push.test;
+
+/**
+ * @author wangwei
+ * @date 2019/4/24
+ */
+public class EasyExcelTest {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/honeywen/push/service/ExcelTest.java
@@ -0,0 +1,9 @@
+package com.honeywen.push.service;
+
+/**
+ * @author wangwei
+ * @date 2019/4/24
+ */
+public class ExcelTest {
+
+}
