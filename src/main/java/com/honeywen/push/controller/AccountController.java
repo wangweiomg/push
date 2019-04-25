5823a4b9f577ae999ae6da171515dac43d748d83
weiw <weiw@opera.com>
2019-04-25 10:12:41 +0800

    add template msg

--- src/main/java/com/honeywen/push/controller/AccountController.java
+++ src/main/java/com/honeywen/push/controller/AccountController.java
@@ -1,12 +1,16 @@
 package com.honeywen.push.controller;
 
+import com.google.common.collect.Lists;
 import com.honeywen.push.entity.Account;
 import com.honeywen.push.service.AccountService;
 import lombok.SneakyThrows;
 import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.mp.api.WxMpKefuService;
 import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.api.WxMpTemplateMsgService;
 import me.chanjar.weixin.mp.bean.kefu.WxMpKefuMessage;
+import me.chanjar.weixin.mp.bean.template.WxMpTemplateData;
+import me.chanjar.weixin.mp.bean.template.WxMpTemplateMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,9 +37,19 @@ public class AccountController {
         String yuanfang = "o7ugQszzrhLLsHKZA4r7x0dgrmRI";
         WxMpKefuMessage message1 = WxMpKefuMessage.TEXT().toUser(wangwei).content("hello world, wangwei").build();
         WxMpKefuMessage message2 = WxMpKefuMessage.TEXT().toUser(yuanfang).content("hello world, yuanfang").build();
+
+        WxMpTemplateData data1 = new WxMpTemplateData("title", "转账提醒");
+        WxMpTemplateData data2 = new WxMpTemplateData("content", "收到拆迁款[4920126.78]元。");
+        List<WxMpTemplateData> list = Lists.newArrayList(data1, data2);
+
+        WxMpTemplateMessage templateMessage1 = WxMpTemplateMessage.builder().templateId("An1aqe9SFEfVG7gWE4C8thJiCMPaeWSl-b397wb_2C8").toUser(wangwei).data(list).build();
+        WxMpTemplateMessage templateMessage2 = WxMpTemplateMessage.builder().templateId("An1aqe9SFEfVG7gWE4C8thJiCMPaeWSl-b397wb_2C8").toUser(yuanfang).data(list).build();
         try {
             wxMpService.getKefuService().sendKefuMessage(message1);
             wxMpService.getKefuService().sendKefuMessage(message2);
+
+            wxMpService.getTemplateMsgService().sendTemplateMsg(templateMessage1);
+            wxMpService.getTemplateMsgService().sendTemplateMsg(templateMessage2);
         } catch (WxErrorException e) {
             e.printStackTrace();
         }
