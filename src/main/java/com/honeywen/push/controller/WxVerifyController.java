5118cd6d3a4c105bb58b4838feb6f7665c4c0f23
weiw <weiw@opera.com>
2019-05-05 19:45:38 +0800

    add verify file

--- src/main/java/com/honeywen/push/controller/WxVerifyController.java
+++ src/main/java/com/honeywen/push/controller/WxVerifyController.java
@@ -6,7 +6,6 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
