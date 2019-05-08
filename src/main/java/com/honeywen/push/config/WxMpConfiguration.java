534ea738aa86609ca89d86e80133d12ebe6761d7
weiw <weiw@opera.com>
2019-05-08 10:05:25 +0800

    add event

--- src/main/java/com/honeywen/push/config/WxMpConfiguration.java
+++ src/main/java/com/honeywen/push/config/WxMpConfiguration.java
@@ -1,11 +1,18 @@
 package com.honeywen.push.config;
 
+import com.google.common.collect.Maps;
+import com.honeywen.push.handler.SubscribeHandler;
+import static me.chanjar.weixin.common.api.WxConsts.*;
 import me.chanjar.weixin.mp.api.WxMpInMemoryConfigStorage;
+import me.chanjar.weixin.mp.api.WxMpMessageRouter;
 import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.api.impl.WxMpServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
 
 /**
  * @author wangwei
@@ -26,6 +33,25 @@ public class WxMpConfiguration {
     @Value("${wx.mp.template1}")
     private String templateOrderNotice;
 
+    private SubscribeHandler subscribeHandler;
+
+    private static Map<String, WxMpMessageRouter> routers = Maps.newHashMap();
+    private static Map<String, WxMpService> mpServices = Maps.newHashMap();
+
+    @Autowired
+    public WxMpConfiguration(SubscribeHandler subscribeHandler) {
+        this.subscribeHandler = subscribeHandler;
+    }
+
+
+    @Bean
+    public WxMpService wxMpService() {
+
+        WxMpService wxMpService = new WxMpServiceImpl();
+        wxMpService.setWxMpConfigStorage(config());
+        return wxMpService;
+
+    }
 
     private WxMpInMemoryConfigStorage config() {
 
@@ -39,12 +65,15 @@ public class WxMpConfiguration {
 
 
     @Bean
-    public WxMpService wxMpService() {
+    public WxMpMessageRouter messageRouter(WxMpService wxMpService) {
+        final WxMpMessageRouter newRouter = new WxMpMessageRouter(wxMpService);
 
-        WxMpService wxMpService = new WxMpServiceImpl();
-        wxMpService.setWxMpConfigStorage(config());
-        return wxMpService;
+        // 关注事件
+        newRouter.rule().async(false).msgType(XmlMsgType.EVENT)
+                .event(EventType.SUBSCRIBE).handler(subscribeHandler)
+                .end();
 
+        return newRouter;
     }
 
 }
--- src/main/java/com/honeywen/push/controller/WxMpController.java
+++ src/main/java/com/honeywen/push/controller/WxMpController.java
@@ -1,6 +1,11 @@
 package com.honeywen.push.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import me.chanjar.weixin.mp.api.WxMpMessageRouter;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.bean.message.WxMpXmlMessage;
+import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -11,6 +16,17 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/wx")
 public class WxMpController {
+
+    private WxMpService wxMpService;
+
+    private WxMpMessageRouter messageRouter;
+
+    @Autowired
+    public WxMpController(WxMpService wxMpService, WxMpMessageRouter messageRouter) {
+        this.wxMpService = wxMpService;
+        this.messageRouter = messageRouter;
+    }
+
 
     @GetMapping("/validateToken/")
     public String validateToken(String signature, String timestamp, String nonce, String echostr) {
@@ -33,6 +49,18 @@ public class WxMpController {
                         + " timestamp=[{}], nonce=[{}], requestBody=[\n{}\n] ",
                 openid, signature, encType, msgSignature, timestamp, nonce, requestBody);
 
+        // 明文传输
+        if (encType == null) {
+
+        }
+
+        return null;
+
+    }
+
+
+    private WxMpXmlOutMessage route(WxMpXmlMessage message) {
+        messageRouter.route(message);
         return null;
 
     }
