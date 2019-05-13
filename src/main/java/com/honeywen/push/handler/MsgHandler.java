0550f89c36d7ccef1a16480422b36a14e8858605
weiw <weiw@opera.com>
2019-05-13 18:14:48 +0800

    dd

--- src/main/java/com/honeywen/push/handler/MsgHandler.java
+++ src/main/java/com/honeywen/push/handler/MsgHandler.java
@@ -47,6 +47,6 @@ public class MsgHandler extends AbstractHandler {
         WxMpXmlOutMessage msg = new TextBuilder().build(content, wxMessage, wxMpService);
 
         log.info("<--msgHandler最后回复-->{}", msg);
-        return msg;
+        return new me.chanjar.weixin.mp.builder.outxml.TextBuilder().content(content).toUser(wxMessage.getFromUser()).build();
     }
 }
